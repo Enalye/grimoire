640921626f91d174edf4f3554d12cfb91970ca3f
Enalye <enalyen@gmail.com>
2023-10-10 00:12:21 +0200

    Revert "Test de module AoT de compilation anticipée"
    This reverts commit f9dc5bfa888ae732c101b49d06fb23dd465e2673.

--- source/grimoire/assembly/bytecode.d
+++ source/grimoire/assembly/bytecode.d
@@ -8,7 +8,6 @@ module grimoire.assembly.bytecode;
 import std.file, std.bitmanip, std.array, std.outbuffer;
 import std.exception : enforce;
 
-import grimoire.assembly.aot;
 import grimoire.assembly.symbol;
 
 /// Correspond à une version du langage. \
@@ -625,12 +624,6 @@ final class GrBytecode {
             symbol.deserialize(buffer);
             symbols[i] = symbol;
         }
-    }
-
-    /// Fonction expérimentale permettant de générer du code en D depuis le bytecode.
-    void aot() {
-        AOTCompiler compiler = new AOTCompiler(this);
-        std.file.write("aot_test.d", compiler.compile());
     }
 
     /// Formate la liste des instructions du bytecode dans un format lisible.
